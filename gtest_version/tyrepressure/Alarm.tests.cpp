7c75daa1fcb520fa19dd038486708d3dd4b49c07
Eduard Ruffert At Home <ruffert@gmail.com>
2017-02-07 20:20:37 +0100

    finishes the AlarmBuilder by refactoring the remaining tests.

--- gtest_version/tyrepressure/Alarm.tests.cpp
+++ gtest_version/tyrepressure/Alarm.tests.cpp
@@ -8,7 +8,6 @@ using namespace ::testing;
 
 TEST(Alarm, alarmIsOnWhenPressureIsTooHigh) {
     SafetyRange *safetyRange = new SafetyRange();
-
     double testPressure = safetyRange->getHighThreshold() + 1;
     ISensor *sensor = new SensorThatProbes(testPressure);
 
@@ -25,7 +24,12 @@ TEST(Alarm, alarmIsOnWhenPressureIsTooLow) {
     SafetyRange *safetyRange = new SafetyRange();
     double testPressure = safetyRange->getLowThreshold() - 1;
     ISensor *sensor = new SensorThatProbes(testPressure);
-    Alarm *alarm = new Alarm(sensor, safetyRange);
+
+    Alarm *alarm = AlarmBuilder::anAlarm()->
+            usingSensor(sensor)->
+            andWithSafetyRange(safetyRange)->
+            build();
+
     alarm->check();
     ASSERT_TRUE(alarm->isAlarmOn());
 }
@@ -34,7 +38,12 @@ TEST(Alarm, alarmIsOnWhenPressureIsOk) {
     SafetyRange *safetyRange = new SafetyRange();
     double testPressure = safetyRange->getMeanThreshold(); // mean of range, should always be ok
     ISensor *sensor = new SensorThatProbes(testPressure);
-    Alarm *alarm = new Alarm(sensor, safetyRange);
+
+    Alarm *alarm = AlarmBuilder::anAlarm()->
+            usingSensor(sensor)->
+            andWithSafetyRange(safetyRange)->
+            build();
+
     alarm->check();
     ASSERT_FALSE(alarm->isAlarmOn());
 }
