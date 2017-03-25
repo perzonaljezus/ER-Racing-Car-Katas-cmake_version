26ba7f95b26fccc3c4a35e9e4136aca52e562a5c
Eduard Ruffert At Home <ruffert@gmail.com>
2017-03-25 20:52:45 +0100

    we analyze the code for obvious tests and write our list of todos

--- gtest_version/tyrepressure/Alarm.tests.cpp
+++ gtest_version/tyrepressure/Alarm.tests.cpp
@@ -9,3 +9,9 @@ TEST(Alarm, Foobar)
     alarm->check();
     ASSERT_EQ(true, alarm->isAlarmOn());
 }
+
+// test alarm is off when pressure is ok
+// test alarm is on when pressure is too high
+// test alarm is on when pressure is too low
+// test alarm is off when pressure on high limit
+// test alarm is off when pressure on low limit
