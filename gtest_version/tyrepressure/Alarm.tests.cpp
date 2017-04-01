9ac2bae3751f7383f3356de867ad381091bcc040
Eduard Ruffert At Home <ruffert@gmail.com>
2017-04-01 13:31:45 +0200

    third version: use gmock in all tests, one by one, making sure that the tests remain green after each baby step change.
    and then remove FakeAlarm.

--- gtest_version/tyrepressure/Alarm.tests.cpp
+++ gtest_version/tyrepressure/Alarm.tests.cpp
@@ -1,5 +1,4 @@
 #include "Alarm.hpp"
-#include "FakeAlarm.h"
 #include "TyrepressureSensorStub.hpp"
 
 using ::testing::Return;
@@ -7,24 +6,25 @@ using ::testing::Return;
 TEST(Alarm, testAlarmIsOffWhenPressureIsOk)
 {
     double pressure = 19; // (high + low) 2
-    FakeAlarm *alarm = new FakeAlarm(pressure);
+
+    TyrepressureSensorStub sensor;
+    EXPECT_CALL(sensor, pressureValue()) .WillOnce(Return(pressure));
+
+    Alarm *alarm = new Alarm(&sensor);
     alarm->check();
-    ASSERT_FALSE(alarm->isAlarmOn());
+    EXPECT_FALSE(alarm->isAlarmOn());
 }
 
 TEST(Alarm, testAlarmIsOnWhenPressureIsTooHigh)
 {
     double pressure = 22; // high+1
-    /* Java allows override during object initialization
-     *
-     * Sensor sensor = new Sensor() {
-        public double popNextPressurePsiValue(){
-            return pressure;
-        }
-    };*/
-    FakeAlarm *alarm = new FakeAlarm(pressure);
+
+    TyrepressureSensorStub sensor;
+    EXPECT_CALL(sensor, pressureValue()) .WillOnce(Return(pressure));
+
+    Alarm *alarm = new Alarm(&sensor);
     alarm->check();
-    ASSERT_TRUE(alarm->isAlarmOn());
+    EXPECT_TRUE(alarm->isAlarmOn());
 }
 
 TEST(Alarm, testAlarmIsOnWhenPressureIsTooLow)
@@ -42,15 +42,23 @@ TEST(Alarm, testAlarmIsOnWhenPressureIsTooLow)
 TEST(Alarm, testAlarmIsOffWhenPressureIsOnHighLimit)
 {
     double pressure = 21; // high
-    FakeAlarm *alarm = new FakeAlarm(pressure);
+
+    TyrepressureSensorStub sensor;
+    EXPECT_CALL(sensor, pressureValue()) .WillOnce(Return(pressure));
+
+    Alarm *alarm = new Alarm(&sensor);
     alarm->check();
-    ASSERT_FALSE(alarm->isAlarmOn());
+    EXPECT_FALSE(alarm->isAlarmOn());
 }
 
 TEST(Alarm, testAlarmIsOffWhenPressureIsOnLowLimit)
 {
     double pressure = 17; // low
-    FakeAlarm *alarm = new FakeAlarm(pressure);
+
+    TyrepressureSensorStub sensor;
+    EXPECT_CALL(sensor, pressureValue()) .WillOnce(Return(pressure));
+
+    Alarm *alarm = new Alarm(&sensor);
     alarm->check();
-    ASSERT_FALSE(alarm->isAlarmOn());
+    EXPECT_FALSE(alarm->isAlarmOn());
 }
--- gtest_version/tyrepressure/FakeAlarm.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Eduard Ruffert on 25/03/2017.
-//
-
-#include "FakeAlarm.h"
-
-double FakeAlarm::pressureValue() {
-    return this->pressure;
-}
